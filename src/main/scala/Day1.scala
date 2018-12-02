03eae37b4e531ecc4951c6d60523f9f59a471700
Milan Bharanya <milan.bharanya@tundra.com>
2018-12-02 14:47:47 +0100

    Second star

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,10 +1,30 @@
+import scala.collection.mutable
 import scala.io.Source
 
 object Day1 extends App {
   var initialValue = 0
   val filename = "day1/input"
-  for (line <- Source.fromFile(filename).getLines) {
-    initialValue += line.toInt
-  }
-  println(initialValue)
+
+  val alreadyFoundMap = mutable.Set[Integer]()
+
+  val lines: List[String] = Source.fromFile(filename).getLines.toList
+
+    Stream.continually(lines.toStream).takeWhile(lines => {
+          lines.toList.exists( line => {
+            val foundVal = line.toInt
+            initialValue += foundVal
+            if (alreadyFoundMap.contains(initialValue)) {
+              println(initialValue)
+              System.exit(0)
+              true
+            } else {
+              alreadyFoundMap += initialValue
+              false
+            }
+          })
+      lines != null
+    }
+    ).mkString(",")
+
+
 }
